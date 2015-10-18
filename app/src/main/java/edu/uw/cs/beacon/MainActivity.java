3dfd6699a82964bc0a9e895d19eaf90d87957ab4
Dominik Moritz <domoritz@gmail.com>
2015-10-17 23:22:36 -0700

    Add stuff

---
+++ app/src/main/java/edu/uw/cs/beacon/MainActivity.java
@@ -0,0 +1,124 @@
+package edu.uw.cs.beacon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+
+import java.util.Collection;
+
+public class MainActivity extends Activity implements BeaconConsumer, MonitorNotifier {
+    protected static final String TAG = "BeaconMonitor";
+
+    private BeaconManager beaconManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        // Add iBeeks parser
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
+
+        // Add parser
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        // Detect the main identifier (UID) frame:
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
+        // Detect the telemetry (TLM) frame:
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("x,s:0-1=feaa,m:2-2=20,d:3-3,d:4-5,d:6-7,d:8-11,d:12-15"));
+        // Detect the URL frame:
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v"));
+
+        beaconManager.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        // Set the two identifiers below to null to detect any beacon regardless of identifiers
+//        Identifier myBeaconNamespaceId = Identifier.parse("0x2f234454f4911ba9ffa6");
+//        Identifier myBeaconInstanceId = Identifier.parse("0x000000000001");
+        Identifier myBeaconNamespaceId = Identifier.parse("");
+        Identifier myBeaconInstanceId = Identifier.parse("");
+        Region region = new Region("my-beacon-region", myBeaconNamespaceId, myBeaconInstanceId, null);
+        beaconManager.setMonitorNotifier(this);
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void didEnterRegion(Region region) {
+        Log.d(TAG, "I detected a beacon in the region with namespace id " + region.getId1() +
+                " and instance id: " + region.getId2());
+    }
+
+    public void didExitRegion(Region region) {
+    }
+
+    public void didDetermineStateForRegion(int state, Region region) {
+    }
+}
